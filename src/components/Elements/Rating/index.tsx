be20f0755698b36ac03234ec1f965627e4c1960b
RODRIGO ALVES <rodrigoalvesbrasileiro@gmail.com>
2021-11-29 18:33:44 -0300

    feat: create rating element

---
+++ src/components/Elements/Rating/index.tsx
@@ -0,0 +1,17 @@
+import { HiOutlineStar, HiStar } from "react-icons/hi";
+import RatingComponent from "react-rating";
+
+export type RatingProps = {
+  rating?: number;
+};
+
+const Rating = ({ rating }: RatingProps) => (
+  <RatingComponent
+    initialRating={rating}
+    emptySymbol={<HiOutlineStar size={18} color="#BABFC7" />}
+    fullSymbol={<HiStar size={19} color="#F2C94C" />}
+    readonly
+  />
+);
+
+export { Rating };
